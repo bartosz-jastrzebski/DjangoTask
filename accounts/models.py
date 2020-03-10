f9461b0f861e78bf4ba2b6d7becb96811bec5135
Mama <bartosz.jastrzebski89@gmail.com>
2020-03-10 20:06:48 +0100

    First try to create user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from random import randint
 
-# Create your models here.
+
+class User(AbstractUser):
+    birthday = models.DateField()
+    number = models.PositiveSmallIntegerField(editable=False)
+
+    def save(self, *args, **kwargs):
+        if not self.number:
+            self.number = randint(1, 100)
+        super().save(*args, **kwargs)
